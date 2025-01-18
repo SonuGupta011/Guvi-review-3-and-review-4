cfb9c46b573d8c6829f12e53667dd48c6f084a9b
SonuGupta011 <Shahsonu8651@gmail.com>
2025-01-18 21:49:09 +0530

    Create CourseController.java

---
+++ CourseController.java
@@ -0,0 +1,31 @@
+package com.lms.controller;
+
+import com.lms.model.Course;
+import com.lms.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PostMapping
+    public Course addCourse(@RequestBody Course course) {
+        return courseService.addCourse(course);
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id);
+    }
+}
